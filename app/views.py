d4ac05f04114433e10fb807047eceda2490bef93
George <zhora.sviridov.05@bk.ru>
2024-11-11 00:18:20 +0500

    second commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from http.client import responses
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import User
 
 
 # Create your views here.
@@ -19,4 +19,15 @@ def currency(request):
     return render(request, 'app/currency.html')
 
 def registration(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        mail = request.POST.get('mailAdress')
+        password = request.POST.get('password')
+        passwordRepeat = request.POST.get('passwordRepeat')
+        if password != passwordRepeat:
+            return HttpResponse("Пароли не совпадают!")
+        else:
+            app_profile = User(login = login, mail = mail, password = password)
+            app_profile.save()
+            return redirect('login')
     return render(request, 'app/registration.html')
